45248c6e4cb5e1f2141e58d310ee5de91446527a
Marco Alfaro <marcosz125@hotmail.com>
2021-02-28 17:34:24 -0600

    Primer commit

---
+++ Semana1/DotNetConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
